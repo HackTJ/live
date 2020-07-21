a454932ec3622ffed07a4e403ce0d7c6dc42d58e
Sumanth Ratna <sumanth@hacktj.org>
2020-07-21 18:45:00 -0400

    Add WebSocket support to /judge/scoreboard

--- hacktj_live/routing.py
+++ hacktj_live/routing.py
@@ -1,6 +1,12 @@
-from channels.routing import ProtocolTypeRouter
-
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import judge.routing
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            judge.routing.websocket_urlpatterns
+        )
+    ),
 })
---
+++ judge/consumers.py
@@ -0,0 +1,26 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+
+class ScoreboardUpdateConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        await self.channel_layer.group_add(
+            'scoreboard',
+            self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            'scoreboard',
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        await self.channel_layer.group_send(
+            'scoreboard',
+            text_data_json
+        )
