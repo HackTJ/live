72c5cd09929237dcb656c6401463a1a20a440a87
Sumanth Ratna <sumanth@hacktj.org>
2020-07-21 19:28:46 -0400

    Set up channels-redis

--- hacktj_live/settings.py
+++ hacktj_live/settings.py
@@ -42,6 +42,15 @@ INSTALLED_APPS = [
     'tailwind',
 ]
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
