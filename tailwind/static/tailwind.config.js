35196fed4107e61624a57ec4e84be9581f878292
Sumanth Ratna <sumanth@hacktj.org>
2020-07-22 10:22:52 -0400

    Fix scoreboard delete button and update footer branding

---
+++ tailwind/static/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    cursor: ['responsive', 'disabled'],
+    backgroundColor: ['responsive', 'hover', 'focus', 'disabled'],
+    fontWeight: ['responsive', 'hover', 'focus', 'disabled'],
+    textColor: ['responsive', 'hover', 'focus', 'disabled']
+  },
+  plugins: [],
+}
