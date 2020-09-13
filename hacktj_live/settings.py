51d08a41cefbfeefe9661e17d835cc4b2afefc9f
Sumanth Ratna <sumanth@hacktj.org>
2020-09-13 14:46:32 -0400

    [compose][dev] Fix env vars

--- hacktj_live/settings.py
+++ hacktj_live/settings.py
@@ -21,18 +21,20 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
-
-ALLOWED_HOSTS = []
 
 INTERNAL_IPS = [
     'localhost',
     '127.0.0.1',
 ]
 
+ALLOWED_HOSTS = [
+    *INTERNAL_IPS,
+    'live.hacktj.org'
+]
 
 # Application definition
 
