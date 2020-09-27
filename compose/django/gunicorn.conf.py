8e6932034d548674a1435da0ea0402b4e12da335
Sumanth Ratna <sumanth@hacktj.org>
2020-09-27 14:27:41 -0400

    [compose] Combine /start.sh and /start-dev.sh in ./compose/django/

--- compose/django/gunicorn.conf.py
+++ compose/django/gunicorn.conf.py
@@ -6,6 +6,7 @@ def trace_on_abort():
     from signal import signal, SIGABRT
     from traceback import format_stack
 
+
     def print_trace(_, frame):
         print(''.join(format_stack(frame)))
 
@@ -25,6 +26,6 @@ proxy_allow_ips = "nginx"
 
 workers = cpu_count() * 2 + 1  # TODO: WORKER TIMEOUT when preload_app = False
 threads = workers  # 2-4 x $(NUM_CORES)
-timeout = 120
-graceful_timeout = 120
+# timeout = 120
+# graceful_timeout = 120
 preload_app = True
--- compose/django/start.sh
+++ compose/django/start.sh
@@ -2,8 +2,14 @@
 
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-python manage.py compress
 
-# uvicorn --host '0.0.0.0' --lifespan 'off' 'hacktj_live.asgi:application'
-gunicorn --config ./compose/django/gunicorn.conf.py 'hacktj_live.asgi:application'
+if [ "$DEBUG" == true ]
+then
+  python manage.py runserver 0.0.0.0:8000
+else
+  python manage.py collectstatic --noinput
+  python manage.py compress
+
+  # uvicorn --host '0.0.0.0' --lifespan 'off' 'hacktj_live.asgi:application'
+  gunicorn --config ./compose/django/gunicorn.conf.py 'hacktj_live.asgi:application'
+fi
