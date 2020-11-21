7dd9c9f08a56e75e6002def466afccad57452acf
Sumanth Ratna <sumanth@hacktj.org>
2020-11-21 14:11:34 -0500

    [compose][judge] Load project fixtures in dev config

--- compose/django/start.sh
+++ compose/django/start.sh
@@ -6,7 +6,10 @@ python manage.py createsuperuser --noinput  # from env vars
 
 if [ "$DEBUG" == true ]
 then
+  python manage.py loaddata 'winners-60'
+
   python manage.py collectstatic --noinput --link
+  python manage.py compress  # django-compressor
 
   python manage.py runserver 0.0.0.0:8000
 else
@@ -14,7 +17,6 @@ else
   python manage.py compress  # django-compressor
   python -m whitenoise.compress static/assets/ ""  # whitenoise
 
-  # uvicorn --host '0.0.0.0' --lifespan 'off' 'hacktj_live.asgi:application'
   ulimit -n 2048
   gunicorn --config ./compose/django/gunicorn.conf.py 'hacktj_live.asgi:application'
 fi
