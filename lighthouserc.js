609feba72b7f1415754a17c8164273883b8f39ed
Sumanth Ratna <sumanth@hacktj.org>
2020-12-01 18:58:45 -0500

    Increase startServerReadyTimeout

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,6 +3,7 @@ module.exports = {
     collect: {
       url: ['http://localhost:1379/'],
       startServerCommand: 'docker-compose -f docker-compose.yml -f docker-compose.prod.yml up',
+      startServerReadyTimeout: 60000,
     },
     upload: {
       target: 'temporary-public-storage',
