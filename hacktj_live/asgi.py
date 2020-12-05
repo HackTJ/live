fa37c7a42d15890e5458fc74a6451eb920970e72
Sumanth Ratna <sumanth@hacktj.org>
2020-12-05 18:37:19 -0500

    Update to Channels 3.x

--- hacktj_live/asgi.py
+++ hacktj_live/asgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 
 import os
 
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hacktj_live.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        # Just HTTP for now. (We can add other protocols later.)
+    }
+)
