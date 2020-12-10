4f50c1de25dffb7eb23dabdfa78975b5a8bcce45
Sumanth Ratna <sumanth@hacktj.org>
2020-12-09 19:19:51 -0500

    [utils] Fix admin approval when only one user type is selected

--- utils/adapters.py
+++ utils/adapters.py
@@ -6,10 +6,10 @@ from allauth.account.utils import send_email_confirmation
 
 class LiveAccountAdapter(DefaultAccountAdapter):
     def new_user(self, request):
-        if any(
-            group in settings.LIVE_ADMIN_USER_APPROVAL
-            for group in request.POST["user_type"]
-        ):
+        user_type = request.POST["user_type"]
+        user_type = [user_type] if isinstance(user_type, str) else user_type
+        assert isinstance(user_type, list)
+        if any(group in settings.LIVE_ADMIN_USER_APPROVAL for group in user_type):
             # admin has to go through and manually
             # set users' active status to True
             user = get_user_model()(is_active=False)
