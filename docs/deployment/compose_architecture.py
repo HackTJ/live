10ffb954aa5112068396de1b233c7fbf57a94d5f
Sumanth Ratna <sumanth@hacktj.org>
2021-01-15 21:19:05 -0500

    [docs][deployment] Fix absolute logo paths  in infrastructure diagram
    https://github.com/mingrammer/diagrams/issues/8, 
    https://github.com/mingrammer/diagrams/issues/26

--- docs/deployment/compose_architecture.py
+++ docs/deployment/compose_architecture.py
@@ -1,10 +1,11 @@
 from diagrams import Diagram, Cluster
 from diagrams.saas.cdn import Cloudflare
 from diagrams.onprem.network import Nginx
-# from diagrams.onprem.container import Docker
 from diagrams.onprem.database import PostgreSQL  # Postgresql
 from diagrams.onprem.inmemory import Memcached, Redis
 from diagrams.programming.framework import Django
+
+# from diagrams.onprem.container import Docker
 
 
 with Diagram(
@@ -12,7 +13,7 @@ with Diagram(
     outformat="svg",
     show=False,
     graph_attr={"bgcolor": "transparent"},
-):
+) as diagram:
     # Docker("component")
     cloudflare = Cloudflare("Cloudflare")
     with Cluster("VM"):
@@ -25,3 +26,16 @@ with Diagram(
                 Redis("Redis"),
             ]
     cloudflare >> nginx >> django
+
+# the resulting SVG contains absolute paths to images that are vendored inside
+# of `diagrams`'s source code so we need to inline SVGs, as suggested by:
+# https://github.com/mingrammer/diagrams/issues/8#issuecomment-633121034
+from scour.scour import scourString as scour_string
+
+filepath = f"{diagram.filename}.{diagram.outformat}"
+
+with open(filepath, "rt") as file:
+    generated_image = file.read()
+
+with open(filepath, "wt") as file:
+    file.write(scour_string(generated_image))
