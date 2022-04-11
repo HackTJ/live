a36419f629056a9afb89554f6818e13088bc938c
Sumanth Ratna <sumanth@hacktj.org>
2022-04-11 19:09:45 -0400

    Revert "[judge] Show project title and description in admin"
    This reverts commit 1d7758ca9070839d61409df483745a2f465b7d56.

--- judge/admin.py
+++ judge/admin.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 from .models import Project, Decision, Annotator
 
 # Register your models here.
-
+admin.site.register(Project)
 admin.site.register(Annotator)
 admin.site.register(Decision)
-
-class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
-
-admin.site.register(Project, ProjectAdmin)
