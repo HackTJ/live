42ccf37627854d2bce024f5b99823dae6fa78904
Sumanth Ratna <sumanth@hacktj.org>
2022-04-11 21:45:48 -0400

    Fix list_display attribute

--- utils/admin.py
+++ utils/admin.py
@@ -1,8 +1,6 @@
-import this
 from django.contrib import admin
 from .models import LiveUser
 from django.contrib.auth.admin import UserAdmin
-from django.db import models
 
 
 def set_active(modeladmin, request, queryset):
@@ -14,7 +12,7 @@ set_active.short_description = "Mark selected users as active"
 
 class LiveUserAdmin(UserAdmin):
     actions = [set_active]
-    UserAdmin.list_display = list(UserAdmin.list_display) + ["is_active"]
+    list_display = list(UserAdmin.list_display) + ["is_active"]
 
 
 # admin.site.unregister(User)
